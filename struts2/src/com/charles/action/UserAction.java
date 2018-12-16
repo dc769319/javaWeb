551a30a59d4f415842b27f261a1e80a466dddc5e
dongchao <dongchao@bigo.sg>
2018-12-16 14:53:15 +0800

    struts2

--- struts2/src/com/charles/action/UserAction.java
+++ struts2/src/com/charles/action/UserAction.java
@@ -15,11 +15,13 @@ public class UserAction extends ActionSupport {
 
     public String add() {
         info = "添加用户信息";
+        //return的值，需要对应struts.xml中result标签的name属性
         return "add";
     }
 
     public String update() {
         info = "更新用户信息";
+        //return的值，需要对应struts.xml中result标签的name属性
         return "update";
     }
 }
